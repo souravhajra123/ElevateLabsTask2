4b0e93e6370c32c361ca3c06e6d10c257ac8507b
Sourav Hajra <souravhajra123@gmail.com>
2025-04-08 11:39:43 +0000

    py code added

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', user=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
